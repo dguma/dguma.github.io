9702f42f7bb7cced83e25e0e9b3bc9a3d58c71d8
Dylan Guma <djguma@live.com>
2021-06-18 11:10:08 -0400

    Landing page create

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 // JavaScript Document
+const overlay = document.querySelector('.overlay');
+
+overlay.addEventListener('click', (e) => {
+	e.target.style.animation = 'none';
+});
+
+overlay.addEventListener('touchstart', (e) => {
+	e.target.style.animation = 'none'; 
+});
